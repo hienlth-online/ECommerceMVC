654a24bf24908eb940ca3dc3c423c521511e47f7
HIENLTH 2023 <hyhien@gmail.com>
2023-12-03 16:19:08 +0700

    Video 3 - Menu

---
+++ ECommerceMVC/Controllers/HangHoaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.Controllers
+{
+	public class HangHoaController : Controller
+	{
+		public IActionResult Index(int? loai)
+		{
+			return View();
+		}
+	}
+}
---
+++ ECommerceMVC/ViewModels/MenuLoaiVM.cs
@@ -0,0 +1,9 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+	public class MenuLoaiVM
+	{
+		public int MaLoai { get; set; }
+		public string TenLoai { get; set; }
+		public int SoLuong { get; set; }
+	}
+}
