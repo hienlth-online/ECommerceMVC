356ad6b766527de781d9bd58e015c6025d0832c3
HIENLTH 2023 <hyhien@gmail.com>
2023-12-03 16:19:08 +0700

    Video 3 - Menu

---
+++ ECommerceMVC/ViewComponents/MenuLoaiViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerceMVC.ViewComponents
+{
+	public class MenuLoaiViewComponent : ViewComponent
+	{
+		private readonly Hshop2023Context db;
+
+		public MenuLoaiViewComponent(Hshop2023Context context) => db = context;
+
+		public IViewComponentResult Invoke()
+		{
+			var data = db.Loais.Select(lo => new MenuLoaiVM
+			{
+				MaLoai = lo.MaLoai,
+				TenLoai = lo.TenLoai,
+				SoLuong = lo.HangHoas.Count
+			}).OrderBy(p => p.TenLoai);
+
+			return View(data); // Default.cshtml
+			//return View("Default", data);
+		}
+	}
+}
