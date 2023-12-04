ee1f89eafa4a576c8f75eb15ff8a49e423e22e7e
HIENLTH 2023 <hyhien@gmail.com>
2023-12-04 22:15:32 +0700

    Show Product list

--- ECommerceMVC/Controllers/HangHoaController.cs
+++ ECommerceMVC/Controllers/HangHoaController.cs
@@ -1,12 +1,58 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceMVC.Data;
+using ECommerceMVC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ECommerceMVC.Controllers
 {
 	public class HangHoaController : Controller
 	{
+		private readonly Hshop2023Context db;
+
+		public HangHoaController(Hshop2023Context conetxt)
+		{
+			db = conetxt;
+		}
+
 		public IActionResult Index(int? loai)
 		{
-			return View();
+			var hangHoas = db.HangHoas.AsQueryable();
+
+			if (loai.HasValue)
+			{
+				hangHoas = hangHoas.Where(p => p.MaLoai == loai.Value);
+			}
+
+			var result = hangHoas.Select(p => new HangHoaVM
+			{
+				MaHh = p.MaHh,
+				TenHH = p.TenHh,
+				DonGia = p.DonGia ?? 0,
+				Hinh = p.Hinh ?? "",
+				MoTaNgan = p.MoTaDonVi ?? "",
+				TenLoai = p.MaLoaiNavigation.TenLoai
+			});
+			return View(result);
+		}
+
+		public IActionResult Search(string? query)
+		{
+			var hangHoas = db.HangHoas.AsQueryable();
+
+			if (query != null)
+			{
+				hangHoas = hangHoas.Where(p => p.TenHh.Contains(query));
+			}
+
+			var result = hangHoas.Select(p => new HangHoaVM
+			{
+				MaHh = p.MaHh,
+				TenHH = p.TenHh,
+				DonGia = p.DonGia ?? 0,
+				Hinh = p.Hinh ?? "",
+				MoTaNgan = p.MoTaDonVi ?? "",
+				TenLoai = p.MaLoaiNavigation.TenLoai
+			});
+			return View(result);
 		}
 	}
 }
---
+++ ECommerceMVC/ViewModels/HangHoaVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerceMVC.ViewModels
+{
+	public class HangHoaVM
+	{
+		public int MaHh { get; set; }
+		public string TenHH { get; set; }
+		public string Hinh { get; set; }
+		public double DonGia { get; set; }
+		public string MoTaNgan { get; set; }
+		public string TenLoai { get; set; }
+	}
+}
